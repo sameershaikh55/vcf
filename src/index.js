ea56d7a96e7ec3e806d24271d00461dd6405d6c9
sameershaikh55 <sameermubeen4@gmail.com>
2021-05-03 16:21:38 +0500

    done

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+// import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import vcf from "./contacts.vcf";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        marginTop: "5rem",
+        width: "100%",
+      }}
+    >
+      <div style={{ padding: "10px 20px", background: "black" }}>
+        <a
+          href={vcf}
+          download
+          style={{ color: "white", textDecoration: "none" }}
+        >
+          Download
+        </a>
+      </div>
+    </div>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
